a50b9531209f422b1fd0f5d54f26daf7136a30e6
Lucas Capdevila <lcapdevila60@gmail.com>
2025-07-23 10:07:59 -0300

    Pagina de inicio reconstruida

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               <Link
-                href="#"
+                href="/"
                 className="px-3 py-2 rounded-md text-sm font-medium hover:bg-orange-700 transition-colors duration-200"
               >
                 Inicio
@@ -101,7 +101,7 @@ export default function Navbar() {
       <div className={`${isMenuOpen ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0'} md:hidden transition-all duration-500 ease-in-out overflow-hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link
-            href="#"
+            href="/"
             className="block px-3 py-2 rounded-md text-base font-medium hover:bg-orange-700 transition-colors duration-200"
           >
             Inicio
